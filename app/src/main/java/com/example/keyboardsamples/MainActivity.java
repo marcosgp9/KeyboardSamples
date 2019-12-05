878e052f2ab8fcb54ee6ed2f8346673f445c1d2b
marna <marcosgalvez@gmail.com>
2019-12-05 16:03:08 +0100

    Exerc

---
+++ app/src/main/java/com/example/keyboardsamples/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.keyboardsamples;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.phone_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.phoneList, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+}
